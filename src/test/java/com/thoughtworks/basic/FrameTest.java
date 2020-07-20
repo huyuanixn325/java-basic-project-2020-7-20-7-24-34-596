45bb0b444f719df6e10206d289f58e448e684da1
huyuanixn325 <huyuanxin0325@163.com>
2020-07-20 20:20:23 +0800

    完成测试方法roll_should_return_5，roll_twotimes_should_return_8

--- src/test/java/com/thoughtworks/basic/FrameTest.java
+++ src/test/java/com/thoughtworks/basic/FrameTest.java
@@ -20,4 +20,15 @@ public class FrameTest {
         Assert.assertEquals(5, score);
     }
 
+    @Test
+    public void roll_twotimes_should_return_8() {
+        //given
+        List<Integer> rolls = new ArrayList<>();
+        GameFrame frame = new GameFrame(rolls);
+        frame.roll(5);
+        frame.roll(3);
+        //when
+        int score = frame.getScore();
+        Assert.assertEquals(8, score);
+    }
 }
